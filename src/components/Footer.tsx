3948419b4d775dd4b976312523b9155b11b22ddc
AlvaroSc <alvarotloc@gmail.com>
2022-03-22 09:36:56 +0100

    Cambiados los links del footer por enlaces simples

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,10 @@
-import { Link } from "react-router-dom";
 const Footer = (): JSX.Element => {
   return (
     <footer className="mt-14 h-2/6 bg-slate-900 w-full border-t-2 flex flex-col justify-center items-center p-10">
       <section className="flex border-b-2">
-          <Link to="https://github.com/Alvarotloc" className="text-white no-underline px-5 cursor-pointer transition-all hover:bg-white hover:text-red-600">GitHub</Link>
-          <Link to="https://www.instagram.com/al_sc22/" className="text-white no-underline px-5 cursor-pointer transition-all hover:bg-white hover:text-red-600">Instagram</Link>
-          <Link to="https://www.linkedin.com/in/%C3%A1lvaro-su%C3%A1rez-cuerva-592280189/" className="text-white no-underline px-5 cursor-pointer transition-all hover:bg-white hover:text-red-600">LinkedIn</Link>
+        <a href="https://github.com/Alvarotloc" className="text-white no-underline px-5 cursor-pointer transition-all hover:bg-white hover:text-red-600">GitHub</a>
+        <a href="https://www.instagram.com/al_sc22/" className="text-white no-underline px-5 cursor-pointer transition-all hover:bg-white hover:text-red-600">Instagram</a>
+        <a href="https://www.linkedin.com/in/%C3%A1lvaro-su%C3%A1rez-cuerva-592280189/" className="text-white no-underline px-5 cursor-pointer transition-all hover:bg-white hover:text-red-600">LinkedIn</a>
       </section>
       <p className="mt-5 text-white font-bold">©Created by: ÁlvaroSC</p>
     </footer>
